02d10636685630732393584ccda13226056f889b
Howard Chen <howard3061@gmail.com>
2025-02-26 23:06:35 -0800

    Smaller! 208

--- main.c
+++ main.c
@@ -1,1 +1,1 @@
-char m[1<<21],*p=m,*r=m+99999;d,o;main(c,v)char**v;{read(open(v[1],0),p,-1);for(;c=*p;p++){o=d=92-c,c-60?c-62?c-43?c-45?c-44?c-46?:putchar(*r):read(0,r,1):--*r:++*r:++r:--r;if(!(c^91|*r)+!(c^93|!*r))while(p+=d,o+=(*p==91)-(*p==93));}}
+char s[9999],*r=s,*p;d,o;main(c,v)char**v;{for(p=v[1];c=*p;p++){o=d=92-c,c-60?c-62?c-43?c-45?c-44?c-46?:putchar(*r):read(0,r,1):--*r:++*r:++r:--r;if(!(c^91|*r)+!(c^93|!*r))while(p+=d,o+=(*p==91)-(*p==93));}}
