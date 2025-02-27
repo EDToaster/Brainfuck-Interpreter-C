32dd003a6baf67a815263541a282171b46a7f449
Howard Chen <howard3061@gmail.com>
2025-02-26 21:59:20 -0800

    Smaller! 256

--- main.c
+++ main.c
@@ -1,2 +1,1 @@
-#include<stdio.h>
-int m[999999],s[999999],*r=m,*p=s;main(c,v)char **v;{FILE *f=fopen(v[1],"r");while(~(*p++=fgetc(f)));for(p=s;c=*p;p++){int d=92-c,o=d;c==60||c==62?r+=c-61:c==43||c==45?*r+=44-c:c==44?*r=getchar():c==46?putchar(*r):0;if(!(c-91&&c-93||(d>=0)-!*r)){do{p+=d;if(*p==91||*p==93)o+=92-*p;}while(o);}}}
+int m[1<<20],*r=m,s[1<<20],*p=s,d,o;main(c,v)char**v;{for(c=open(v[1],0);read(c,p,1);p++);for(p=s;c=*p;p++){d=92-c,o=d,c-60?c-62?c-43?c-45?c-44?c-46?:putchar(*r):read(0,r,1):--*r:++*r:++r:--r;if(!(c^91|*r)+!(c^93|!*r)){while(p+=d,o+=(*p==91)-(*p==93));}}}
