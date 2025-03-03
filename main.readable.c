6fc8f5872449f386501ccc35e6c27fe528a1eae2
EDToaster <howard3061@gmail.com>
2025-03-02 22:18:31 -0800

    Housekeeping

--- main.readable.c
+++ main.readable.c
@@ -1,18 +1,17 @@
-// Combined tape and program array
-// size = 2_097_152
-char m[1<<21], 
-// program counter
-*p = m, 
-// memory cell
-*r=m+99999;
-d, o;
-
+char *p;
+s[9999], *r = s, d, o;
 main(c, v) char **v;
 {
-  read(open(v[1],0),p,-1);
-  for(;c=*p;p++){
+  for (p = v[1]; c = *p; p++) {
     o = d = 92 - c,
-    c-60?c-62?c-43?c-45?c-44?c-46?:putchar(*r):read(0,r,1):--*r:++*r:++r:--r;
-    if(!(c^91|*r)+!(c^93|!*r)) while (p+=d,o+=(*p==91)-(*p==93));
+    c - 60 ? c - 62 ? c - 43 ? c - 45 ? c - 44 ? c - 46 ?: putchar(*r)
+                                               : read(0, r, 1)
+                                      : --*r
+                             : ++*r
+                    : ++r
+           : --r;
+    if (!(c ^ 91 | *r) + !(c ^ 93 | !*r))
+      while (p += d, o += (*p == 91) - (*p == 93))
+        ;
   }
 }
